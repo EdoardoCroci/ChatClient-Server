3455619786bd91c716c547be1fa34cc0818a20b2
AndreaLotti <andrealotti88@gmail.com>
2020-10-27 16:44:27 +0100

    Aggiunto in output lista utenti connessi in entrambi i client, sia alla connessione che alla disconnessione

--- src/main/java/server/GestioneChat.java
+++ src/main/java/server/GestioneChat.java
@@ -25,7 +25,7 @@ public class GestioneChat{
                 System.exit(1);
             }
     }
-  
+
     public static void main(String[] args) {
         GestioneChat tcpServer = new GestioneChat();
         tcpServer.start();
@@ -56,6 +56,7 @@ public class GestioneChat{
             outVersoClient = new DataOutputStream(clientS.getOutputStream());
             outVersoClient.writeBytes("Inserisci username" + '\n');
             usernameClient = inDalClient.readLine();
+
             if(utenti.isEmpty()) utenti.add(usernameClient);
             else{
                 for(int i = 0; i < utenti.size(); i++){
@@ -64,14 +65,32 @@ public class GestioneChat{
                         usernameClient = inDalClient.readLine();
                     }
                 }
+                utenti.add(usernameClient);
             }
-                System.out.println(usernameClient + " connesso");
-                outVersoClient.writeBytes(usernameClient + " connesso" + '\n');
+
+            System.out.println(usernameClient + " connesso");
+            outVersoClient.writeBytes(usernameClient + " connesso" + '\n' + '\n');
+            for(int i = 0; i < sockets.size(); i++){
+                sockets.get(i).outVersoClient.writeBytes("Utenti connessi:" + '\n');
+                for(int j = 0; j < utenti.size(); j++) {
+                    sockets.get(i).outVersoClient.writeBytes("Utente:" + utenti.get(j) + '\n');
+                }
+                sockets.get(i).outVersoClient.writeBytes("\n");
+            }
+
             for(;;) {
                 String mex = inDalClient.readLine();
                 if(mex.equalsIgnoreCase("FINE")){
                     System.out.println(usernameClient + ": utente disconnesso" + '\n');
+                    utenti.remove(usernameClient);
                     sockets.remove(this);
+                    for(int i = 0; i < sockets.size(); i++){
+                        sockets.get(i).outVersoClient.writeBytes("Utenti connessi:" + '\n');
+                        for(int j = 0; j < utenti.size(); j++) {
+                            sockets.get(i).outVersoClient.writeBytes("Utente:" + utenti.get(j) + '\n');
+                        }
+                        sockets.get(i).outVersoClient.writeBytes("\n");
+                    }
                     break;
                 }
                 else if (sockets.size() > 1) {
@@ -82,6 +101,7 @@ public class GestioneChat{
                 }
                 else outVersoClient.writeBytes("Nessuno è connesso" + '\n');
             }
+
             outVersoClient.close();
             inDalClient.close();
             clientS.close();
